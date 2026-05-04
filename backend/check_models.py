c2b905bfda32a3118fefd4f6b8fbbf82c72fe77e
Alexsik76 <o.sikorskyi.vn@gmail.com>
2026-05-04 22:52:24 +0300

    rm check_models

--- backend/check_models.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    import models.kvm_node
-    import models.user
-    import models.user_node_permission
-    print("Models imported successfully")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
